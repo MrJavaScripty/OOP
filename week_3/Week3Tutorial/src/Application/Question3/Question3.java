96aa223af41c42d642fbc7a816c11d166b7dd263
ryankuruppu <kuruppr@westminster.ac.uk>
2022-10-12 01:14:14 +0530

    Added Question3 and Main.java

--- week_3/Week3Tutorial/src/Application/Question3/Question3.java
+++ week_3/Week3Tutorial/src/Application/Question3/Question3.java
@@ -10,4 +10,19 @@ public class Question3 implements Question {
         Rectangle r1 = new Rectangle(0, 0, 100, 50);
         System.out.println(r1);
     }
+
+    public void run_2(){
+        Rectangle r1 = new Rectangle(0, 0, 100, 50);
+        System.out.println(r1);
+    }
+
+    public void run_3(){
+        double n1 = 150;
+        double n2 = n1;
+
+        n2 = n2 * 20; // grow n2
+
+        System.out.println(n1);
+        System.out.println(n2);
+    }
 }
